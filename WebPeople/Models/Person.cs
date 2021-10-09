4f76420a66d8451741fedc5e4059bd7a539c932a
Linz Vaca <a2020115786@estudiantes.upsa.edu.bo>
2021-10-09 07:51:37 -0400

    Agregar archivos de proyecto.

---
+++ WebPeople/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebPeople.Models
+{
+    public enum SexType
+    {
+        Female = 0,
+        Male = 1,
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        [Required(ErrorMessage = "El campo del nombre es requerido")]
+        [StringLength(60,MinimumLength = 5,ErrorMessage = "El nombre tiene que tener entre 5 a 60 caracteres")]
+        [Display(Name = "Nombre completo")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Sexo")]
+        public SexType Sex { get; set; }
+        [Display(Name = "Pasatiempos")]
+        public string Hobbie { get; set; }
+
+    }
+}
